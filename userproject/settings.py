94652977329b12e0b23e491f85ff9223f49e73b4
Aman Shrivastav <76123848+Aman2620@users.noreply.github.com>
2024-01-03 22:29:09 +0530

    Initial Commit

--- userproject/settings.py
+++ userproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@s^kh(*sjo#s9u%hi@b6c%5qe!@qp6o^-(d)q%km9pn3!0r#!2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
